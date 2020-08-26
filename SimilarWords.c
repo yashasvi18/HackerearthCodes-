5cb7886f89ad1de74116ecba35d8fbdc9ad6500b
yashasvi <yashi1812@gmail.com>
2020-08-26 19:10:29 +0530

    You are required to enter a word that consists of x and y that denote the number of Zs and Os respectively. The input word is considered similar to word zoo if z*x =y
    Determine if the entered word is similar to word zoo.
    
    For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.

---
+++ SimilarWords.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	char a[100];
+	scanf("%s",a);
+	int cz=0;
+	int co=0;            			
+	for(int i =0;i< strlen(a);i++)
+	{
+		if(a[i] == 'z' || a[i] == 'Z')
+		{
+			cz =cz+1;
+		}
+		if (a[i]=='o' || a[i] == 'O')
+		{
+			co = co + 1;
+		}
+	}
+	if(2*cz == co)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+
+	 
+}
